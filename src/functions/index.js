16b736c783847ff415de1e5527e04dbdea29aa4f
Nikhil Sawant <nikhilsawant1011@gmail.com>
2025-08-04 08:21:02 +0000

    issue is still not resolved

--- src/functions/index.js
+++ src/functions/index.js
@@ -82,7 +82,6 @@ exports.createConversation = functions.https.onCall(async (data, context) => {
         const otherUserData = otherUserDoc.data() || {};
         
         const newConversation = {
-            id: conversationId,
             participants: sortedParticipants,
             participantNames: {
                 [currentUserId]: currentUserData.displayName || currentUserData.email || 'User',
