2aee2a08fc0452b8b566023a7fb16add8f8470e0
Nikhil Sawant <nikhilsawant1011@gmail.com>
2025-08-04 08:28:14 +0000

    intercept-console-error.ts:40 Error creating or fetching conversation: F

--- src/functions/index.js
+++ src/functions/index.js
@@ -59,8 +59,7 @@ exports.createConversation = functions.https.onCall(async (data, context) => {
     const db = admin.firestore();
 
     try {
-        const participants = [currentUserId, otherUserId];
-        const sortedParticipants = [...participants].sort();
+        const sortedParticipants = [currentUserId, otherUserId].sort();
         const conversationId = sortedParticipants.join('_');
         const conversationRef = db.collection('conversations').doc(conversationId);
         
@@ -79,8 +78,8 @@ exports.createConversation = functions.https.onCall(async (data, context) => {
             throw new functions.https.HttpsError('not-found', 'One or more users not found.');
         }
 
-        const currentUserData = currentUserDoc.data() || {};
-        const otherUserData = otherUserDoc.data() || {};
+        const currentUserData = currentUserDoc.data();
+        const otherUserData = otherUserDoc.data();
         
         const newConversation = {
             id: conversationId,
